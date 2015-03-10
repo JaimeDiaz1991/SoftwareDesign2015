c74b8fdcb61d158c8796fd0f5365d4d3752c8e74
isidrojesus.delgado@gmail.com <isidrojesus.delgado@gmail.com@db35111e-9298-5b6b-7474-cf45dd8c624b>
2015-03-10 17:51:06 +0000

    Modificadas clases (BARAJA,CARTA,MATCHBLACKJACK).
    Echar un vistazo al constructor de MatchBlackJack

--- trunk/JEG/src/com/maco/juegosEnGrupo/server/dominio/Baraja.java
+++ trunk/JEG/src/com/maco/juegosEnGrupo/server/dominio/Baraja.java
@@ -1,5 +1,37 @@
 package com.maco.juegosEnGrupo.server.dominio;
 
+import java.util.ArrayList;
+
 public class Baraja {
 
+	public Carta cartas[];
+	
+	public void Baraja(){
+		
+		ArrayList<String> palos = new ArrayList<String>();
+		palos.add("picas");
+		palos.add("diamantes");
+		palos.add("corazones");
+		palos.add("treboles");
+		boolean figura;
+		for(int i = 0; i < palos.size(); i++){
+			for(int j = 0; j < 12; j++){
+				if(j >= 10)
+					figura = true;
+				else
+					figura = false;
+				cartas[i] = new Carta(palos.get(i), j, figura);
+			}
+		}
+	}
+
+	public Carta[] getCartas() {
+		return cartas;
+	}
+
+	public void setCartas(Carta[] cartas) {
+		this.cartas = cartas;
+	}
+	
+	
 }
